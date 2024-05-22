b01ba502e7ea0aa1e4cbb69e6d34732ef8c64649
JohnQuimson <john.quimson777@gmail.com>
2024-05-22 17:04:07 +0100

    feat: filter by genre & return toString

--- main.js
+++ main.js
@@ -32,7 +32,9 @@ const data = [
   },
 ];
 
+// ---------------------------
 // MOVIE
+// ---------------------------
 class Movie {
   constructor(title, year, genre, rating, type) {
     this.title = title;
@@ -47,7 +49,9 @@ class Movie {
   }
 }
 
+// ---------------------------
 // TV SERIES
+// ---------------------------
 class TvSerie extends Movie {
   constructor(title, year, genre, rating, type, seasons) {
     super(title, year, genre, rating, type);
@@ -59,7 +63,9 @@ class TvSerie extends Movie {
   }
 }
 
+// ---------------------------
 // NEW ARRAY
+// ---------------------------
 const dataInstance = data.map((element) => {
   // type = movie
   if (element.type === 'movie') {
@@ -84,9 +90,13 @@ const dataInstance = data.map((element) => {
   }
 });
 
-console.log(dataInstance.toString());
+// ForEach per stampare l'istanza con gli elementi
+dataInstance.forEach((instance) => console.log(instance.toString()));
+console.log('');
 
+// ---------------------------
 // Rating by Genre function
+// ---------------------------
 function RatingByGenre(movies, genre) {
   // filtro i film in base al genere
   const filterMovie = movies.filter((movie) => movie.genre === genre);
@@ -97,15 +107,16 @@ function RatingByGenre(movies, genre) {
   return sumRating / filterMovie.length;
 }
 
-// Test della funzione
 const averageDramaRating = RatingByGenre(dataInstance, 'Drama');
 const averageScifiRating = RatingByGenre(dataInstance, 'Sci-Fi');
-console.log('');
+
 console.log(`Media voti per il genere Drama è: ${averageDramaRating}`);
 console.log(`Media voti per il genere Sci-Fi è: ${averageScifiRating}`);
 console.log('');
 
+// ---------------------------
 // Genres list
+// ---------------------------
 function getGenre(array) {
   const noRepeatGenre = [];
   array.forEach((element) => {
@@ -118,3 +129,21 @@ function getGenre(array) {
 
 const noRepeatGenre = getGenre(dataInstance);
 console.log('Genres:', noRepeatGenre.toString());
+console.log('');
+
+// ---------------------------
+// FILM FILTER BY GENRE, return toString
+// ---------------------------
+function filterByGenre(movies, genre) {
+  const filterMovie = movies
+    // creo nuovo array con solo elementi di quel genere
+    .filter((movie) => movie.genre === genre)
+    // dall'array filtrato ne creo un altro applicando toString
+    .map((movie) => movie.toString());
+
+  return filterMovie;
+}
+
+const ScifiMovies = filterByGenre(dataInstance, 'Sci-Fi');
+console.log('Film di genere Sci-Fi:', ScifiMovies);
+console.log('');
