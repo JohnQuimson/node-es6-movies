b9d84904bccea867579d10b030678da91ecb6bd9
JohnQuimson <john.quimson777@gmail.com>
2024-05-22 14:34:41 +0100

    feat: scaffold

---
+++ main.js
@@ -0,0 +1,33 @@
+const data = [
+  { title: 'Jaws', year: 1975, genre: 'Drama', rating: 8, type: 'movie' },
+  {
+    title: 'Breaking Bad',
+    year: 2008,
+    genre: 'Drama',
+    rating: 9.5,
+    type: 'tv',
+    seasons: 5,
+  },
+  {
+    title: 'Inception',
+    year: 2010,
+    genre: 'Sci-Fi',
+    rating: 8.8,
+    type: 'movie',
+  },
+  {
+    title: 'Game of Thrones',
+    year: 2011,
+    genre: 'Fantasy',
+    rating: 9.3,
+    type: 'tv',
+    seasons: 8,
+  },
+  {
+    title: 'Interstellar',
+    year: 2014,
+    genre: 'Sci-Fi',
+    rating: 8.6,
+    type: 'movie',
+  },
+];
